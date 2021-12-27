55da1ea3ab29f4aac2b85504a3fb4640c33793d7
TeluguSravani <telugusravani2@gmail.com>
2021-12-27 19:17:48 +0530

    sixth commit

---
+++ prime.java
@@ -0,0 +1,10 @@
+ class Prime {
+public static void main(String[] args){
+ int i;
+for(int i=1;i<=100;i++)
+if(n%2 ==0)
+System.out.println("given num is not prime");
+else
+System.out.println("given num is prime ");
+ }
+}
