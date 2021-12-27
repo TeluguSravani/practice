76c8d100df0eba49b8b3d664446023b357579f96
TeluguSravani <telugusravani2@gmail.com>
2021-12-27 19:08:00 +0530

    fourth commit

---
+++ main1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+Class Main{
+public static void(String[] args){
+Scanner scan =new Scanner(system.in)
+int maxLaneSpeed =scan.nextInt();
+Lane 1=new Lane(maxspeed);
+
+Scan.close();
+;
+Switch ():
+case 1:
+System.out.println("speed of lane and car");
+break;
+case 2:
+System.out.println("Increase of car speed 10 km");
+break;
+case 3:
+System.out.println("decrease of car speed
