e8640cf1437a2af328aa925a606ae623a2725881
TeluguSravani <telugusravani2@gmail.com>
2021-12-27 19:02:21 +0530

    second commit

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+class ifelse{
+public static void main(String[] args){
+int n=100;
+if(n%2=0)
+{
+elseif(n>2&&n<5)
+elseif(n>20)
+System.out.println("not wierd");
+}
+else
+{
+if(n>6&&n<20)
+System.out.println("wired");
+}
+}
