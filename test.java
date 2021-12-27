7adebfc7eb779a8b9bc60a0f9369a7f558900fae
TeluguSravani <telugusravani2@gmail.com>
2021-12-27 19:19:53 +0530

    seventh commit

---
+++ test.java
@@ -0,0 +1,5 @@
+class test{
+public static void main(String[] args){
+String str="this is my sentence ";
+String output = str.substring(0,1) to uppercase;
+System.out.println(Captilise("this is my sentence"));
